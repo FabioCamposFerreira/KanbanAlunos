b1f471929d88af4087da0b2a97091386d389dfc9
Fábio Campos <63975718+FabioCamposFerreira@users.noreply.github.com>
2025-09-02 19:28:35 -0300

    Update data.js

--- data.js
+++ data.js
@@ -10,6 +10,9 @@ const alunos = [
     {
         nome: "Felipe",
         tarefas: [
+            { titulo: "FeedBack 02/09/25", descricao: "", status: "pendente", pontos: 1 },
+            { titulo: "Site portfólio no GitHub", descricao: "", status: "pendente", pontos: 2 },
+            { titulo: "Feedback sobre o portfólio", descricao: "", status: "pendente", pontos: 1 },
             { titulo: "Dailly 02/09/25", descricao: "", status: "finalizado", pontos: 1 },
             { titulo: "Video: Portifolio", descricao: "https://www.youtube.com/watch?v=Vr3hWm7My24", status: "finalizado", pontos: 2 },
             { titulo: "FeedBack 26/08/25", descricao: "", status: "finalizado", pontos: 1 },
@@ -22,6 +25,9 @@ const alunos = [
     {
         nome: "Eduardo",
         tarefas: [
+            { titulo: "FeedBack 02/09/25", descricao: "", status: "pendente", pontos: 1 },
+            { titulo: "Site portfólio no GitHub", descricao: "", status: "pendente", pontos: 2 },
+            { titulo: "Feedback sobre o portfólio", descricao: "", status: "pendente", pontos: 1 },
             { titulo: "Dailly 02/09/25", descricao: "", status: "finalizado", pontos: 1 },
             { titulo: "Video: Curriculo", descricao: "https://www.youtube.com/watch?v=q652jp6YVqk&pp=ygUNY3VycmljdWxvIGRldg%3D%3D", status: "pendente", pontos: 1 },
             { titulo: "FeedBack 19/08/25", descricao: "", status: "finalizado", pontos: 1 },
@@ -31,6 +37,9 @@ const alunos = [
     {
         nome: "Vitor",
         tarefas: [
+            { titulo: "FeedBack 02/09/25", descricao: "", status: "pendente", pontos: 1 },
+            { titulo: "Site portfólio no GitHub", descricao: "", status: "pendente", pontos: 2 },
+            { titulo: "Feedback sobre o portfólio", descricao: "", status: "pendente", pontos: 1 },
             { titulo: "Dailly 02/09/25", descricao: "", status: "finalizado", pontos: 1 },
             { titulo: "Video: Portifolio", descricao: "https://www.youtube.com/watch?v=Vr3hWm7My24", status: "finalizado", pontos: 2 },
             { titulo: "FeedBack 26/08/25", descricao: "", status: "finalizado", pontos: 1 },
@@ -43,6 +52,9 @@ const alunos = [
     {
         nome: "Arthur",
         tarefas: [
+            { titulo: "FeedBack 02/09/25", descricao: "", status: "pendente", pontos: 1 },
+            { titulo: "Site portfólio no GitHub", descricao: "", status: "pendente", pontos: 2 },
+            { titulo: "Feedback sobre o portfólio", descricao: "", status: "finalizado", pontos: 1 },
             { titulo: "Dailly 02/09/25", descricao: "", status: "finalizado", pontos: 1 },
             { titulo: "Video: Portifolio", descricao: "https://www.youtube.com/watch?v=Vr3hWm7My24", status: "finalizado", pontos: 2 },
             { titulo: "Fazer Curriculo", descricao: "Canvas.com", status: "finalizado", pontos: 2 },
@@ -55,6 +67,9 @@ const alunos = [
     {
         nome: "Luis",
         tarefas: [
+            { titulo: "FeedBack 02/09/25", descricao: "", status: "pendente", pontos: 1 },
+            { titulo: "Site portfólio no GitHub", descricao: "", status: "pendente", pontos: 2 },
+            { titulo: "Feedback sobre o portfólio", descricao: "", status: "pendente", pontos: 1 },
             { titulo: "Dailly 02/09/25", descricao: "", status: "finalizado", pontos: 1 },
             { titulo: "Video: Portifolio", descricao: "https://www.youtube.com/watch?v=Vr3hWm7My24", status: "finalizado", pontos: 2 },
             { titulo: "FeedBack 26/08/25", descricao: "", status: "finalizado", pontos: 1 },
