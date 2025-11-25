6422dc6c32a87b0398c8d6eb90dfbe94d89ea72c
Fábio Campos <63975718+FabioCamposFerreira@users.noreply.github.com>
2025-11-25 22:07:52 +0000

    1

--- data.js
+++ data.js
@@ -5,6 +5,7 @@ const alunos = [
         tarefas: [
             { titulo: "Projeto Site de noticas usando JSON", descricao: "", status: "pendente", pontos: 3 },
             { titulo: "Video: Curriculo", descricao: "https://www.youtube.com/watch?v=q652jp6YVqk&pp=ygUNY3VycmljdWxvIGRldg%3D%3D", status: "pendente", pontos: 1 },
+            { titulo: "FeedBack 18/11/25", descricao: "", status: "finalizado", pontos: 1 },
             { titulo: "Dailly 18/11/25", descricao: "", status: "finalizado", pontos: 1 },
             { titulo: "FeedBack 04/11/25", descricao: "", status: "finalizado", pontos: 1 },
             { titulo: "Video JSON", descricao: "", status: "finalizado", pontos: 1 },
@@ -29,8 +30,8 @@ const alunos = [
         nome: "Felipe",
         tarefas: [
             { titulo: "Projeto Site de noticas usando JSON", descricao: "", status: "pendente", pontos: 3 },
-            { titulo: "Feedback sobre o portfólio", descricao: "", status: "pendente", pontos: 1 },
             { titulo: "Video JSON", descricao: "", status: "pendente", pontos: 1 },
+            { titulo: "Feedback sobre o portfólio", descricao: "", status: "pendente", pontos: 1 },
             { titulo: "Portfólio CSS", descricao: "", status: "pendente", pontos: 10 },
             { titulo: "Site portfólio no GitHub", descricao: "", status: "finalizado", pontos: 2 },
             { titulo: "Dailly 18/11/25", descricao: "", status: "finalizado", pontos: 1 },
@@ -160,6 +161,8 @@ const alunos = [
             { titulo: "Projeto Hamburgueria CSS", descricao: "", status: "pendente", pontos: 3 },
             { titulo: "Projeto Hamburgueria JS", descricao: "", status: "pendente", pontos: 3 },
             { titulo: "Projeto Hamburgueria HTML", descricao: "", status: "pendente", pontos: 3 },
+            { titulo: "Dailly 25/11/25", descricao: "", status: "finalizado", pontos: 1 },
+            { titulo: "FeedBack 18/11/25", descricao: "", status: "finalizado", pontos: 1 },
             { titulo: "Dailly 18/11/25", descricao: "", status: "finalizado", pontos: 1 },
             { titulo: "FeedBack 11/11/25", descricao: "", status: "finalizado", pontos: 1 },
             { titulo: "FeedBack 04/11/25", descricao: "", status: "finalizado", pontos: 1 },
@@ -202,7 +205,8 @@ const alunos = [
     {
         nome: "Natan",
         tarefas: [
-            { titulo: "Video Lista de Tarefas", descricao: "", status: "pendente", pontos: 2 },
+            { titulo: "FeedBack 18/11/25", descricao: "", status: "finalizado", pontos: 1 },
+            { titulo: "Video Lista de Tarefas", descricao: "", status: "finalizado", pontos: 2 },
             { titulo: "Dailly 18/11/25", descricao: "", status: "finalizado", pontos: 1 },
             { titulo: "FeedBack 11/11/25", descricao: "", status: "finalizado", pontos: 1 },
             { titulo: "FeedBack 04/11/25", descricao: "Fazer commits e criar repositórios no GitHub", status: "finalizado", pontos: 1 },
